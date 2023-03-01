4d3a6c6e8b1dc1e7e7ca6f5e64a4937b9ff437a8
AlyssonVanelli <97549937+AlyssonVanelli@users.noreply.github.com>
2023-03-01 09:13:00 -0300

    new dates

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,32 @@ import './App.css'
 
 function App() {
   const [day, hour, minute, second] = useContdown("Mar 8, 2023 12:00:00")
+  const [day2, hour2, minute2, second2] = useContdown("Sep 8, 2023 12:00:00")
+  const [day3, hour3, minute3, second3] = useContdown("Nov 8, 2023 12:00:00")
 
   return (
     <div className="App" style={{ backgroundImage: `url(${israel}` }} >
       <div className="container">
-        <Title title='Contagem regressiva para viagem' />
+        <Title title='Israel' />
         <div className="countdown-container">
           <Counter title='Dias' number={day} />
           <Counter title='Horas' number={hour} />
           <Counter title='Minutos' number={minute} />
           <Counter title='Segundos' number={second} />
+        </div>
+        <Title title='Selichot' />
+        <div className="countdown-container">
+          <Counter title='Dias' number={day2} />
+          <Counter title='Horas' number={hour2} />
+          <Counter title='Minutos' number={minute2} />
+          <Counter title='Segundos' number={second2} />
+        </div>
+        <Title title='Polônia' />
+        <div className="countdown-container">
+          <Counter title='Dias' number={day3} />
+          <Counter title='Horas' number={hour3} />
+          <Counter title='Minutos' number={minute3} />
+          <Counter title='Segundos' number={second3} />
         </div>
       </div>
     </ div>
